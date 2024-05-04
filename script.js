9b31906c78f3d1d0ee5eb8296b9297235ac96ed2
jamessouth65 <158448383+jamessouth65@users.noreply.github.com>
2024-05-04 01:20:03 -0400

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+const arr = []
+
+const firstname = document.getElementById(firstname) 
+const lastname = document.getElementById(lastname);
+const areacode = document.getElementById(areacode);
+const number = document.getElementById(number);
+
